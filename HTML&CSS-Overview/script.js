446723c118accc5bb030e9e0e7c9ff13f28a7fdb
Nasreen Arif <nasreen_arif@live.com>
2024-09-12 14:34:15 -0400

    class activity

--- HTML&CSS-Overview/script.js
+++ HTML&CSS-Overview/script.js
@@ -3,14 +3,10 @@
 
 // console.log(age);
 
-
 // const user={
 //     name:"Nasreen",
 //     age:33
 // }
-
-
-
 
 // const user1=user;
 
@@ -18,24 +14,28 @@
 
 // console.log(user1.age)
 
-
 // let message="Hello World";
 // let newMessage=message;
-
 
 // message="Hello Class!";
 
 // console.log(message);
 // console.log(newMessage);
 
-
-
 // import api, { apiKeys } from "./keys.js";
 
-import * as keys from "./keys.js";
+// import * as keys from "./keys.js";
 
-console.log(keys.default);
+// console.log(keys.default);
 
 // console.log(apiKeys);
 // console.log(api);
 
+function greeting(){
+    alert("Hello from JS class!")
+}
+
+let age=33;
+let checkAge= age==30 ? true : false;
+
+console.log(checkAge);
