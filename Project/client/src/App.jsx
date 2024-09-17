46254f4a6133e09e5515633f5486fc3e3861671d
Nasreen Arif <nasreen_arif@live.com>
2024-09-17 16:02:58 -0400

    react class monstration props, children props, hooks

--- Project/client/src/App.jsx
+++ Project/client/src/App.jsx
@@ -1,16 +1,45 @@
 import { useState } from "react";
 import "./App.css";
 import Image from "./components/Image";
-
-
+import PropComponent from "./components/PropComponent";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  let content;
+  if(count==0){
+    content=<p>Count is 0</p>
+  }
+  else
+  {
+    content=<p>Count has update</p>
+  }
+
   return (
     <>
-    <Image></Image>
+    {content}
+      <Image></Image>
       <h1>Vite + React</h1>
+
+      <PropComponent
+        title="Web Programming"
+        description="Discussing Component with prop in today's class"
+      ><p>Demonstration of children prop</p>
+      <p>test children</p></PropComponent>
+
+
+{/* <PropComponent
+        title="test"
+        description="Discussing Component with prop in today's class"
+      >
+        
+      </PropComponent>
+
+<PropComponent
+        title="test2"
+        description="Discussing Component with prop in today's class"
+      ></PropComponent> */}
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
