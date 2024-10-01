e9cd62a55dd481c834abcddbc5ba733a8c1f26df
Nasreen Arif <nasreen_arif@live.com>
2024-10-01 16:39:10 -0400

    class demonstration of 01/10

--- Project/client/src/App.jsx
+++ Project/client/src/App.jsx
@@ -1,58 +1,17 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Image from "./components/Image";
-import PropComponent from "./components/PropComponent";
-
+import Login from "./components/Login";
+import Dashboard from "./components/dashboard";
 function App() {
-  const [count, setCount] = useState(0);
-
-  let content;
-  if(count==0){
-    content=<p>Count is 0</p>
-  }
-  else
-  {
-    content=<p>Count has update</p>
-  }
 
   return (
-    <>
-    {content}
-      <Image></Image>
-      <h1>Vite + React</h1>
-
-      <PropComponent
-        title="Web Programming"
-        description="Discussing Component with prop in today's class"
-      ><p>Demonstration of children prop</p>
-      <p>test children</p></PropComponent>
-
-
-{/* <PropComponent
-        title="test"
-        description="Discussing Component with prop in today's class"
-      >
-        
-      </PropComponent>
-
-<PropComponent
-        title="test2"
-        description="Discussing Component with prop in today's class"
-      ></PropComponent> */}
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+    <Route path="/" element={<Login/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
+    </Routes>
   );
 }
 
 export default App;
+
+
